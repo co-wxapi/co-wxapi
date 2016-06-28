371575555a5a01a8cad7b316e3ea813f0cd8247f
unidevel <unidevel@hotmail.com>
2016-06-29 06:21:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+var WxBase = require('co-wxbase');
+var MODULES = {
+  auth: 'co-wxauth',
+  pay : 'co-wxpay',
+  jsapi: 'co-wxjsapi',
+  token: 'co-wxtoken',
+  msg : 'co-wxmsg',
+  qrcode: 'co-wxqrcode'
+}
+
+class WxAPI extends WxBase{
+  constructor(config){
+    super(config);
+    // Initial access token
+    var modules = config.modules || ['auth', 'token', 'jsapi', 'pay', 'qrcode', 'msg'];
+    modules.forEach((mod)=>{
+      var modFile = MODULES[mod];
+      this[mod] = require(modFile)(config);
+    });
+    this.modules = modules;
+  }
+
+  setTokenProvider(provider){
+    this.modules.forEach((mod)=>{
+      this[mod].setTokenProvider(provider);
+    });
+  }
+}
+
+module.exports = function(config){
+  var wxapi = new WxAPI(config);
+  return wxapi;
+}
